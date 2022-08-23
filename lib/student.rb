e9d81be6bea5c68a3c6938990ff894db987db757
James White <whitejaad@gmail.com>
2022-08-23 14:54:04 -0700

    "Initial commit"

--- lib/student.rb
+++ lib/student.rb
@@ -1,2 +1,13 @@
-singers = ["billie", "ariana", "lizzo", "leo"]
-p singers
+class Student
+   attr_reader :name,
+               :cookies
+
+   def initialize(name)
+    @name = name
+    @cookies = []
+   end
+
+   def add_cookie(cookie)
+     @cookies << cookie
+   end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,13 +1,34 @@
+require 'rspec'
+require './lib/student'
+
 # Add the 2 requires you will need here
-
-
-describe Student do
+RSpec.describe Student do
   describe '#initialize' do
-    it 'is an instance of student' do
-      # write the code to initialize a new student object
+    it 'exists' do
+      # what we think will happen
+      #condition to be fulfilled
+       student = Student.new('Penelope')
+
+       expect(student).to be_instance_of Student
     end
+    it 'has attributes' do
+      student = Student.new('Penelope')
+      expect(student.name).to eq('Penelope')
+      expect(student.cookies).to eq([])
+    end
+  end
+  describe '#add_cookie' do
+    it 'can add cookies' do
+      student = Student.new('Penelope')
+      expect(student.cookies).to eq([])
+
+      student.add_cookie('Chocolate Chip')
+      expect(student.cookies).to eq(['Chocolate Chip'])
+    end
+end
+end 
+      # write the code to initialize a new student object
+
     # test it has a name
     # test it has cookies
     # test it can add cookies
-  end
-end
